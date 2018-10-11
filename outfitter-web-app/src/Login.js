1524b950fe94212f9bc1dff27589260ab1f7e7eb
DIGNITAS\bgotay <bgotay@dignitastechnologies.com>
2018-10-11 16:04:57 -0400

    OFR-8 Styles for the login page have been fixed, click handlers were added for the buttons, and change handlers were added for the field

--- outfitter-web-app/src/Login.js
+++ outfitter-web-app/src/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './Login.css';
 import { Card, CardActions, CardContent, TextField, Button, Grid } from '@material-ui/core';
 import { getMainTheme } from './Themes';
+import {MuiThemeProvider} from '@material-ui/core/styles'
+
+//region Styling
 
 const mainTheme = getMainTheme();
+
+document.body.style.setProperty('--input-focus-border', mainTheme.palette.secondary.main);
 
 const style = {
     card: {
@@ -13,7 +19,8 @@ const style = {
 
     title: {
         fontSize: "3em",
-        cursor: "default"
+        cursor: "default",
+        color: mainTheme.palette.primary.light
     },
 
     fieldText: {
@@ -38,9 +45,19 @@ const style = {
         backgroundColor: mainTheme.palette.primary.main, 
         float: "right", 
         width: "100%"
-    }
-}
+    },
 
+    inputLabels: {
+        shrink: mainTheme.overrides.MuiInputLabel.shrink
+    }
+};
+
+//endregion
+
+var username;
+var password;
+
+/** Tags for the login widget */
 const loginPage = 
     <Grid item>
         <Card style={style.card}>
@@ -48,23 +65,53 @@ const loginPage =
                 <label style={style.title}>Login</label>
 
                 <form noValidate>
-
-                    <TextField label="Username" autoFocus={true} margin="normal" inputProps={style.fieldText} fullWidth={true} variant="outlined"/>
-                    <TextField label="Password" margin="normal" type="password" inputProps={style.fieldText} fullWidth={true} variant="outlined"/>
+                    <MuiThemeProvider theme={getMainTheme}>
+                        <TextField onChange={event => {onLoginFieldChange(event.target.value)}} className="loginField" label="Username" autoComplete="username" 
+                                    margin="normal" inputProps={style.fieldText} fullWidth={true} variant="outlined"/>
+                                    
+                        <TextField onChange={event => {onPasswordFieldChange(event.target.value)}} className="loginField" label="Password" autoComplete="password" 
+                                    margin="normal" type="password" inputProps={style.fieldText} fullWidth={true} variant="outlined"/>
+                    </MuiThemeProvider>
                 </form>
             </CardContent>
             <CardActions style={style.cardAction}>
                 <Grid container spacing={8}>
                     <Grid item xs={12} sm={12} md={6}>
-                        <Button size="large" style={style.registerButton}>Register</Button>
+                        <Button onClick={onRegisterClick} size="large" style={style.registerButton}>Register</Button>
                     </Grid>
                     <Grid item xs={12} sm={12} md={6}>
-                        <Button size="large" style={style.loginButton}>Login</Button>
+                        <Button onClick={onLoginClick} size="large" style={style.loginButton}>Login</Button>
                     </Grid>
                 </Grid>
             </CardActions>
         </Card>
     </Grid>
+
+/** Handles changes to the login field */
+function onLoginFieldChange(text) {
+    username = text;
+}
+
+/** Handles changes to the password field */
+function onPasswordFieldChange(text) {
+    password = text
+}
+
+/** Click handler for register button */
+function onRegisterClick() {
+    console.log("Register Clicked");
+    // TODO: Navigate to Register page
+}
+
+/** Click handler for login button */
+function onLoginClick() {
+    console.log("Login Clicked");
+
+    if(username == null || password == null || username.length == 0 || password.length == 0) {
+        console.log("Username and password cannot be empty");
+    }
+    // TODO: Contact server and wait for a response
+}
 
 export function getLoginPage() {
     return loginPage;
--- outfitter-web-app/src/Themes.js
+++ outfitter-web-app/src/Themes.js
@@ -1,20 +1,36 @@
 import {createMuiTheme} from '@material-ui/core/styles'
 import {grey, lightGreen} from '@material-ui/core/colors'
 
+const palette = {
+    primary: {
+        light: grey[600],
+        main: grey[800],
+        dark: grey[900],
+      },
+      secondary: {
+          main: lightGreen[500],
+      },
+      text: {
+        primary: "rgba(255, 255, 255, 255)",
+      },
+}
+
 const mainTheme = createMuiTheme({
-    // The light and dark variants of the colors are generated automatically.
-    // You can set them explicitely if you'd like though.
-    palette: {
-        primary: {
-          main: grey[800],
+    palette: palette,
+
+    overrides: {
+        MuiInputLabel: {
+            root: {
+                color: palette.primary.light,
+            },
+
+            shrink: {
+                '&$shrink': {
+                    transform: "translate(14px, -10px) scale(0.75)!important",
+                }
+            },
         },
-        secondary: {
-            main: lightGreen[500],
-        },
-        text: {
-          primary: "rgba(255, 255, 255, 255)",
-        },
-    },
+    }
   });
 
 export function getMainTheme() { 
